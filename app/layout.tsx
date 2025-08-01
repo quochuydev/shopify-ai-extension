48bebfaea43acafce44b351bb2ca7d586f04ec53
quochuydev <quochuy.dev@gmail.com>
2025-08-01 09:52:29 +0700

    chore: update web metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,9 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Shopify AI Extension",
+  description:
+    "AI-powered Chrome extension that automatically generates Shopify product details from images using drag and drop",
 };
 
 const geistSans = Geist({
